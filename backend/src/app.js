fb1a95b4786b05f47767484cc64fd08dd4878531
n-qber <nmkbertozzo@gmail.com>
2020-03-28 00:00:13 -0300

    end yay

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const { errors } = require('celebrate')
 const cors = require('cors');
 
 const app = express();
@@ -8,4 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.use(errors());
+
+module.exports = app;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,20 +4,46 @@ const SessionController = require('./controllers/SessionController');
 const OngController = require('./controllers/OngController');
 const express = require('express');
 const routes = express.Router();
+const { celebrate, Segments, Joi} = require('celebrate');
 
 
 routes.post('/sessions', SessionController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+routes.post('/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({authorization: Joi.string().required()}).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required()})}), IncidentController.create);
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete);
 
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
 
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 
 
 module.exports = routes;
